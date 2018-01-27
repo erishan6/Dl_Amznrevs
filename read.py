2c00cff12d48e7a1c7e312e5d6b8aa4730a045d2
Ishan Adhaulia <erishan6@gmail.com>
2018-01-27 19:55:03 +0100

    Writing diff file for pos & neg

--- read.py
+++ read.py
@@ -1,39 +1,22 @@
 import xml.etree.ElementTree as ET
-from gensim.models.doc2vec import LabeledSentence
-from gensim.models import Doc2Vec
+import re
 
 DATA_FOLDER = 'SentimentClassification/'
 languages = ['en']#, 'fr', 'de']
 domains = ['books', 'dvd', 'music']
 
 def parsingData():
-	for language in languages:
-		for domain in domains:
-			tree = ET.parse(DATA_FOLDER + language + '/' + domain + '/' + 'train.review')
-        	root = tree.getroot()
-        	for item in root:
-        		print(item[1].text)
-
-def doc2vec_source(train_size):
-    f = open("d2v_train.txt", 'w')
     for language in languages:
-    	for domain in domains:
-    		tree = ET.parse(DATA_FOLDER + language + '/' + domain + '/' + 'train.review')
-        	root = tree.getroot()
-        	for item in root:
-        		f.write(item[2].text+"\n")
-    f.close()
-
-def create_doc2vec_model(vectorsize):
-    sources = {'d2v_train.txt':'TRAIN'}
-    sentences = LabeledLineSentence(sources)
-    model = Doc2Vec(min_count=1, window=10, size=vectorsize, sample=1e-4, negative=5, workers=cores,alpha=0.025, min_alpha=0.025)
-    model.build_vocab(sentences.to_array())
-    print('Starting to train...')
-    for epoch in range(10):
-        print('Epoch ',epoch)
-        model.train(sentences.sentences_perm())
-    model.save('./amazon.doc2vec')
-    return model
-
+        for domain in domains:
+            tree_head  =  DATA_FOLDER + language + '/' + domain + '/'
+            pos = open(tree_head+"pos.txt", 'w')
+            neg = open(tree_head+"neg.txt", 'w')
+            tree = ET.parse(tree_head+'train.review')
+            root = tree.getroot()
+            for item in root:
+                #print(item[1].text)
+                if float(item[1].text) == ( 1 or 2):
+                    neg.write(re.sub( '\s+', ' ', (item[2].text).strip())+"\n")
+                elif float(item[1].text) == ( 4 or 5):
+                    pos.write(re.sub( '\s+', ' ', (item[2].text).strip())+"\n")
 parsingData()
