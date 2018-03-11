1288cfc8c95feec9adb40b9ca757deefd13ced06
Ishan Adhaulia <erishan6@gmail.com>
2018-03-11 11:37:00 +0100

    in progress 2, dann

--- dhruv/DannModel.py
+++ dhruv/DannModel.py
@@ -26,7 +26,7 @@ class DannModel(object):
     """Simple MNIST domain adaptation model."""
     def __init__(self, vocab_size):
         self._build_model(vocab_size)
-    
+
     def _build_model(self, vocab_size):
         # pixel_mean = np.vstack([loadDataForDANN(["books"], "train"), loadDataForDANN(["music"], "train")]).mean((0,1,2))
         # X should be in the form of embedding vector
@@ -38,10 +38,10 @@ class DannModel(object):
         self.domain = tf.placeholder(tf.float32, [None, 2], name="domain")
         self.l = tf.placeholder(tf.float32, [], name="l")
         self.train = tf.placeholder(tf.bool, [], name="train")
-        
+
         # X_input = (tf.cast(self.X, tf.float32))# - pixel_mean) / 255.
 
-        # 
+        #
         embedding_size = 128
         # X = tf.placeholder(tf.int32, [None, sequence_length], name='X')  # Input data
 
@@ -59,9 +59,9 @@ class DannModel(object):
         embedded_chars = tf.nn.embedding_lookup(W, self.X)
         embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
         print(embedded_chars_expanded.shape)
-        
-        # 
-        
+
+        #
+
         # CNN model for feature extraction
         with tf.variable_scope('feature_extractor'):
 
@@ -69,28 +69,28 @@ class DannModel(object):
             b_conv0 = bias_variable([32])
             h_conv0 = tf.nn.relu(conv2d(embedded_chars_expanded, W_conv0) + b_conv0)
             h_pool0 = max_pool_2x2(h_conv0)
-            
+
             W_conv1 = weight_variable([5, 5, 32, 48])
             b_conv1 = bias_variable([48])
             h_conv1 = tf.nn.relu(conv2d(h_pool0, W_conv1) + b_conv1)
             h_pool1 = max_pool_2x2(h_conv1)
-            
+
             # The domain-invariant feature
             self.feature = tf.reshape(h_pool1, [-1, 40 * 40 * 48])
-            
+
         # MLP for class prediction
         with tf.variable_scope('label_predictor'):
-            
+
             # Switches to route target examples (second half of batch) differently
             # depending on train or test mode.
             all_features = lambda: self.feature
             source_features = lambda: tf.slice(self.feature, [0, 0], [batch_size // 2, -1])
             classify_feats = tf.cond(self.train, source_features, all_features)
-            
+
             all_labels = lambda: self.y
             source_labels = lambda: tf.slice(self.y, [0, 0], [batch_size // 2, -1])
             self.classify_labels = tf.cond(self.train, source_labels, all_labels)
-            
+
             W_fc0 = weight_variable([40 * 40 * 48, 100])
             b_fc0 = bias_variable([100])
             h_fc0 = tf.nn.relu(tf.matmul(classify_feats, W_fc0) + b_fc0)
@@ -102,28 +102,28 @@ class DannModel(object):
             W_fc2 = weight_variable([100, 2])
             b_fc2 = bias_variable([2])
             logits = tf.matmul(h_fc1, W_fc2) + b_fc2
-            
+
             self.pred = tf.nn.softmax(logits)
             self.pred_loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=self.classify_labels)
 
         # Small MLP for domain prediction with adversarial loss
         with tf.variable_scope('domain_predictor'):
-            
+
             # Flip the gradient when backpropagating through this operation
             feat = flip_gradient(self.feature, self.l)
-            
+
             d_W_fc0 = weight_variable([40 * 40 * 48, 100])
             d_b_fc0 = bias_variable([100])
             d_h_fc0 = tf.nn.relu(tf.matmul(feat, d_W_fc0) + d_b_fc0)
-            
+
             d_W_fc1 = weight_variable([100, 2])
             d_b_fc1 = bias_variable([2])
             d_logits = tf.matmul(d_h_fc0, d_W_fc1) + d_b_fc1
-            
+
             self.domain_pred = tf.nn.softmax(d_logits)
             self.domain_loss = tf.nn.softmax_cross_entropy_with_logits(logits=d_logits, labels=self.domain)
 
-def train_and_evaluate(training_mode, graph, model, xs, ys, num_steps=8600, verbose=False):
+def train_and_evaluate(training_mode, graph, model, xs, ys, xt,yt, num_steps=8600, verbose=True):
     """Helper to run the model with different training modes."""
 
     with tf.Session(graph=graph) as sess:
@@ -131,16 +131,16 @@ def train_and_evaluate(training_mode, graph, model, xs, ys, num_steps=8600, verb
 
         # Batch generators
         gen_source_batch = batch_generator([xs, ys], batch_size // 2)
-        gen_target_batch = batch_generator([xs, ys], batch_size // 2)
+        gen_target_batch = batch_generator([xt, yt], batch_size // 2)
         gen_source_only_batch = batch_generator([xs, ys], batch_size)
-        gen_target_only_batch = batch_generator([xs, ys], batch_size)
+        gen_target_only_batch = batch_generator([xt, yt], batch_size)
 
         domain_labels = np.vstack([np.tile([1., 0.], [batch_size // 2, 1]),
                                    np.tile([0., 1.], [batch_size // 2, 1])])
 
         # Training loop
         for i in range(num_steps):
-            
+
             # Adaptation param and learning rate schedule as described in the paper
             p = float(i) / num_steps
             l = 2. / (1. + np.exp(-10. * p)) - 1
@@ -159,7 +159,7 @@ def train_and_evaluate(training_mode, graph, model, xs, ys, num_steps=8600, verb
                     feed_dict={model.X: X, model.y: y, model.domain: domain_labels,
                                model.train: True, model.l: l, learning_rate: lr})
 
-                if verbose and i % 100 == 0:
+                if verbose and i % 1 == 0:
                     print('loss: {}  d_acc: {}  p_acc: {}  p: {}  l: {}  lr: {}'.format(
                             batch_loss, d_acc, p_acc, p, l, lr))
 
@@ -183,13 +183,13 @@ def train_and_evaluate(training_mode, graph, model, xs, ys, num_steps=8600, verb
         target_acc = sess.run(label_acc,
                             feed_dict={model.X: mnistm_test, model.y: mnist.test.labels,
                                        model.train: False})
-        
+
         test_domain_acc = sess.run(domain_acc,
                             feed_dict={model.X: combined_test_imgs,
                                        model.domain: combined_test_domain, model.l: 1.0})
-        
+
         test_emb = sess.run(model.feature, feed_dict={model.X: combined_test_imgs})
-        
+
     return source_acc, target_acc, test_domain_acc, test_emb
 
 
@@ -201,19 +201,20 @@ if __name__ == '__main__':
     # Build the model graph
     graph = tf.get_default_graph()
     xs, ys, vs1 = data(["music"])
+    xt,yt,vs2 = data(["books"])
     print(ys[0])
     with graph.as_default():
-        model = DannModel(vs1)
-        
+        model = DannModel(max(vs1,vs2))
+
         learning_rate = tf.placeholder(tf.float32, [])
-        
+
         pred_loss = tf.reduce_mean(model.pred_loss)
         domain_loss = tf.reduce_mean(model.domain_loss)
         total_loss = pred_loss + domain_loss
 
         regular_train_op = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(pred_loss)
         dann_train_op = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(total_loss)
-        
+
         # Evaluation
         correct_label_pred = tf.equal(tf.argmax(model.classify_labels, 1), tf.argmax(model.pred, 1))
         label_acc = tf.reduce_mean(tf.cast(correct_label_pred, tf.float32))
@@ -221,7 +222,7 @@ if __name__ == '__main__':
         domain_acc = tf.reduce_mean(tf.cast(correct_domain_pred, tf.float32))
 
     print('\nDomain adaptation training')
-    source_acc, target_acc, d_acc, dann_emb = train_and_evaluate('dann', graph, model, xs, ys)
+    source_acc, target_acc, d_acc, dann_emb = train_and_evaluate('dann', graph, model, xs, ys,xt,yt)
     print('Source (MNIST) accuracy:', source_acc)
     print('Target (MNIST-M) accuracy:', target_acc)
     print('Domain accuracy:', d_acc)
