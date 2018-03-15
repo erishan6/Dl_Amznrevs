00c8fa85182c90d5cd3e2bc03994d094758c4e8a
Dhruv Mishra <ubiquitiousdm@gmail.com>
2018-03-16 00:00:46 +0100

    fixing value error

--- dhruv/evalDannModel.py
+++ dhruv/evalDannModel.py
@@ -77,26 +77,29 @@ def evalDann():
             batches_x = read.batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
             batches_y = read.batch_iter(list(y_test), FLAGS.batch_size, 1, shuffle=False)
             # Collect the predictions here
-            all_predictions = []
+            all_predictions = [[0,0], [0,0]]
             for x_test_batch, y_test_batch in zip(batches_x,batches_y):
                 # test_x = np.vstack([x_test_batch])
                 # test_y = np.vstack([y_test_batch])
                 # batch_predictions = sess.run(predictions,{X: test_x,y :test_y,train :False})
                 batch_predictions = sess.run(predictions,{X: x_test_batch,y :y_test_batch,train:False})
-                print(batch_predictions)
-                # all_predictions = np.concatenate((all_predictions, batch_predictions),axis=0)
+                # print(batch_predictions)
+                # print(all_predictions)
+                all_predictions = np.concatenate((all_predictions, batch_predictions),axis=0)
+
+            all_predictions = all_predictions[2:]
 
     # Print accuracy if y_test is defined
-    # if y_test is not None:
-    #     correct_predictions = float(sum(all_predictions == y_test))
-    #     print("Total number of test examples: {}".format(len(y_test)))
-    #     print("Accuracy: {:g}".format(correct_predictions/float(len(y_test))))
+    if y_test is not None:
+        correct_predictions = float(sum(all_predictions == y_test))
+        print("Total number of test examples: {}".format(len(y_test)))
+        print("Accuracy: {:g}".format(correct_predictions/float(len(y_test))))
 
-    # # Save the evaluation to a csv
-    # predictions_human_readable = np.column_stack((np.array(x_raw), all_predictions))
-    # out_path = os.path.join(FLAGS.checkpoint_dir, "..", "prediction.csv")
-    # print("Saving evaluation to {0}".format(out_path))
-    # with open(out_path, 'w') as f:
-    #     csv.writer(f).writerows(predictions_human_readable)
+    # Save the evaluation to a csv
+    predictions_human_readable = np.column_stack((np.array(x_raw), all_predictions))
+    out_path = os.path.join(FLAGS.checkpoint_dir, "..", "prediction.csv")
+    print("Saving evaluation to {0}".format(out_path))
+    with open(out_path, 'w') as f:
+        csv.writer(f).writerows(predictions_human_readable)
 
 evalDann()
